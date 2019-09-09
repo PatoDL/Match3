a1c5246800104992b16a4633399f802959c50c40
Unknown <unknown@example.com>
2019-09-09 12:36:25 -0300

    Added project

---
+++ Mach3/Assets/Scenes/Control.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    Model m;
+
+    public GameObject panel;
+
+    View v;
+
+    void Start()
+    {
+        m = new Model();
+
+        m.InitData(width, height);
+
+        v = panel.GetComponent<View>();
+
+        v.DrawGrid(m.zarlanga, width, height);
+    }
+}
