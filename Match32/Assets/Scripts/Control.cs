dca78f1712e88fa6b5646f44fd0cffb068b20eb6
Pato <patodl2701@gmail.com>
2019-09-30 00:06:24 -0300

    Added touch controls and controller improved

--- Match32/Assets/Scripts/Control.cs
+++ Match32/Assets/Scripts/Control.cs
@@ -8,7 +8,7 @@ public class Control : MonoBehaviour
     public int width;
     public int height;
 
-
+    public int score = 0;
 
     public GameObject BaseGO;
 
@@ -83,16 +83,72 @@ public class Control : MonoBehaviour
     int s2I;
     int s2J;
 
+    public bool canInteract = true;
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+#if UNITY_STANDALONE || UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0) && canInteract)
         {
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.zero);
 
             if (hit)
             {
-                if (!selected1)
+                for (int i = 0; i < width; i++)
                 {
+                    for (int j = 0; j < height; j++)
+                    {
+                        if (hit.transform.gameObject == gridObjs[i, j])
+                        {
+                            selected1 = hit.transform.gameObject;
+                            s1I = i;
+                            s1J = j;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        if(Input.GetMouseButtonUp(0) && canInteract)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.zero);
+
+            if (hit)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    for (int j = 0; j < height; j++)
+                    {
+                        if (hit.transform.gameObject == gridObjs[i, j])
+                        {
+                            selected2 = hit.transform.gameObject;
+                            s2I = i;
+                            s2J = j;
+
+                            if (!ChangeTile(s1I, s1J, s2I, s2J))
+                                ChangeTile(s2I, s2J, s1I, s1J);
+
+                            break;
+                        }
+                    }
+                }
+                selected1 = null;
+                selected2 = null;
+            }
+        }
+#endif
+#if UNITY_ANDROID && !UNITY_EDITOR
+        if(Input.touchCount>0 && canInteract)
+        {
+            Touch t = Input.GetTouch(0);
+
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(t.position), Vector2.zero);
+
+            if(hit)
+            {
+                switch(t.phase)
+                {
+                case TouchPhase.Began:
                     for (int i = 0; i < width; i++)
                     {
                         for (int j = 0; j < height; j++)
@@ -106,9 +162,8 @@ public class Control : MonoBehaviour
                             }
                         }
                     }
-                }
-                else
-                {
+                    break;
+                case TouchPhase.Ended:
                     for (int i = 0; i < width; i++)
                     {
                         for (int j = 0; j < height; j++)
@@ -119,80 +174,135 @@ public class Control : MonoBehaviour
                                 s2I = i;
                                 s2J = j;
 
-                                if (ChangeTile(s1I, s1J, s2I, s2J))
-                                {
-                                    Debug.Log("si");
-                                }
-                                else
-                                    Debug.Log("no");
-
-                                //DrawGrid();
+                                if (!ChangeTile(s1I, s1J, s2I, s2J))
+                                    ChangeTile(s2I, s2J, s1I, s1J);
 
                                 break;
                             }
                         }
                     }
-
+                    break;
+                case TouchPhase.Canceled:
                     selected1 = null;
                     selected2 = null;
+                    break;
                 }
             }
+            selected1 = null;
+            selected2 = null;
         }
+#endif
+
+        if (ReviewData(false, false) || ReviewData(true, false))
+        {
+            canInteract = false;
+            PassData();
+            DrawGrid();
+        }
+        else
+            canInteract = true;
     }
 
     bool ChangeTile(int i, int j, int i2, int j2)
     {
         bool changed = false;
 
-        Destroy(gridObjs[i, j]);
-        gridObjs[i, j] = null;
+        if (gridObjs[i, j] != null)
+        {
+            Destroy(gridObjs[i, j]);
+            gridObjs[i, j] = null;
+        }
 
-        Destroy(gridObjs[i2, j2]);
-        gridObjs[i2, j2] = null;
+        if (gridObjs[i, j] != null)
+        {
+            Destroy(gridObjs[i2, j2]);
+            gridObjs[i2, j2] = null;
+        }
 
         int sValue = grid[i, j];
         grid[i, j] = grid[i2, j2];
         grid[i2, j2] = sValue;
 
-        return changed = ReviewData(false, false) || ReviewData(true, false);
+        DrawGrid();
+
+        bool hor = ReviewData(false, false);
+        bool ver = ReviewData(true, false);
+
+        return changed = (hor || ver);
     }
 
-    void Erase3(int i, int j, bool vertical)
+    void Erase3(int i,int j, bool vertical)
     {
-        for (int d = 0; d < 3; d++)
+        if(!vertical)
         {
-            if (!vertical)
+            for (int d = 0; d < 3; d++)
             {
-                Destroy(gridObjs[i - d, j]);
-                gridObjs[i - d, j] = null;
-                PassData(i - d, j);
+                if (gridObjs[i-d, j] != null && (i - d < width) && (i - d >= 0))
+                {
+                    Destroy(gridObjs[i - d, j]);
+                    gridObjs[i - d, j] = null;
+                    score += 10;
+                }
             }
-            else
+        }
+        else
+        {
+            for (int d = 0; d < 3; d++)
             {
-                Destroy(gridObjs[j, i - d]);
-                gridObjs[j, i - d] = null;
-                PassData(j, i - d);
+                if (gridObjs[j, i - d] != null && (i-d < width) && (i - d >= 0))
+                {
+                    Destroy(gridObjs[j, i - d]);
+                    gridObjs[j, i - d] = null;
+                    score += 10;
+                }
             }
         }
     }
 
-    void PassData(int i, int j)
+    void PassData()
     {
-        while(j < width)
+        for (int i = 0; i < width; i++)
         {
-            if (j + 1 < width)
-                grid[i, j] = grid[i, j + 1];
-            else
-                grid[i, j] = Random.Range(0, 4);
+            for (int j = 0; j < height; j++)
+            {
+                if (gridObjs[i, j] == null)
+                {
+                    grid[i, j] = -1;
+                }
+            }
+        }
 
-            j++;
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if(grid[i, j] == -1)
+                {
+                    int k = j+1;
+                    while (k < width)
+                    {
+                        if(grid[i, k] != -1)
+                        {
+                            grid[i, j] = grid[i, k];
+                            grid[i, k] = -1;
+                            break;
+                        }
+                        else
+                        {
+                            k++;
+                        }
+                    }
+                    if (k >= width)
+                        grid[i, j] = Random.Range(0, 4);
+                }
+            }
         }
     }
 
     bool ReviewData(bool vertical, bool changeRep)
     {
         int aValue;
-        int pValue = -1;
+        int pValue = -2;
         int repCounter = 1;
         bool stillChanging = false;
 
@@ -201,14 +311,22 @@ public class Control : MonoBehaviour
             for(int i=0;i<width;i++)
             {
                 if (!vertical)
+                {
                     aValue = grid[i, j];
+                }
                 else
+                {
                     aValue = grid[j, i];
+                }
 
                 if (aValue == pValue)
+                {
                     repCounter++;
+                }
                 else
+                {
                     repCounter = 1;
+                }
 
                 if(repCounter>=3)
                 {
@@ -241,9 +359,7 @@ public class Control : MonoBehaviour
                             Debug.Log(j + "-" + (i - 1));
                             Debug.Log(j + "-" + i);
                         }
-                        DrawGrid();
                         Erase3(i, j, vertical);
-                        DrawGrid();
                     }
                     stillChanging = true;
                     repCounter = 1;
@@ -254,46 +370,4 @@ public class Control : MonoBehaviour
         }
         return stillChanging;
     }
-
-    //bool ReviewVerticalData(bool changeRep)
-    //{
-    //    int aValue;
-    //    int pValue = -1;
-    //    bool stillChanging = false;
-    //    int repCounter = 1;
-
-    //    for(int i=0;i<width;i++)
-    //    {
-    //        for(int j=0;j<height;j++)
-    //        {
-    //            aValue = grid[i, j];
-
-    //            if (aValue == pValue)
-    //                repCounter++;
-    //            else
-    //                repCounter = 1;
-
-    //            if (repCounter>=3)
-    //            {
-    //                if(changeRep)
-    //                {
-    //                    int iter = 0;
-    //                    while(aValue==pValue)
-    //                    {
-    //                        iter++;
-    //                        aValue = Random.Range(0, 3);
-    //                        if (iter > 25)
-    //                            aValue++;
-    //                    }
-    //                    grid[i, j] = aValue;
-    //                }
-    //                stillChanging = true;
-    //                repCounter = 1;
-    //            }
-    //            pValue = aValue;
-    //        }
-    //        repCounter = 1;
-    //    }
-    //    return stillChanging;
-    //}
 }
